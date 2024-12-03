dcd07a4da13e7091d0580bde48a7467d080622b7
mlevanic <levanicmario@gmail.com>
2024-12-03 15:55:10 +0100

    third commit

---
+++ src/app/performance/page.tsx
@@ -0,0 +1,6 @@
+export default function PerformancePage() {
+    return (
+      <div>Performance</div>
+    );
+  }
+  
---
+++ src/app/scale/page.tsx
@@ -0,0 +1,4 @@
+export default function ScalePage() {
+    return <div>Scale</div>;
+  }
+  
